c1750353a4e7335de45be9d8f56f965628c5fe42
rblument <rblument@regis.edu>
2025-09-02 15:29:18 -0600

    Initial commit

---
+++ src/main/java/edu/regis/merc/MercApp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package edu.regis.merc;
+
+/**
+ *
+ * @author rickb
+ */
+public class MercApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
